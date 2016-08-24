425d2e79632cedfa13dee2bfc0357e09d868d56e
Dustin Essington <dustin.essington@rackspace.com>
2016-08-24 15:14:53 -0500

    Added Nintendo Network ID with !getnintendo and !setnintendo

--- discord_bot.js
+++ discord_bot.js
@@ -938,6 +938,58 @@ var commands = {
     }
   },
   
+  "setnintendo": {
+    usage: "[network id]",
+    description: "Save your Nintendo Network ID so others can find you",
+    process: function(bot,msg,suffix){
+
+      if(suffix){
+        var user = msg.author.id;
+        setUserMeta(user,'nintendo',suffix);
+        bot.sendMessage(msg.channel, 'set your Nintendo Network ID to: ' + suffix);
+      }
+    }
+  },
+
+  "getnintendo": {
+    usage: "[@user]",
+    description: "Gets the PlayStation Network name of a user",
+    process : function(bot,msg,suffix) {
+
+      if(!suffix) {
+        return; // return early, stop execution.
+      }
+
+      var users = suffix.split(' ');
+      
+      for (var i = 0; i < users.length; i++) {
+        
+        var id;
+      
+        // Server nickname detection
+        if(users[i].substr(2, 1) === "!") {
+          id = users[i].slice(3, users[i].length - 1);
+        } else {
+          id = users[i].slice(2, users[i].length - 1);
+        }
+        
+        queryUserMeta(id, function(data) {
+          var message = "";
+          
+          if(data.nintendo == null) {
+            message = "Sorry, I dont have their Nintendo Network ID. :frowning:";
+          }
+          else {
+            message = "Here's the Nintendo Network ID name for " + tagUser(data) + ":\n" +  data.nintendo;
+          }
+          if(msg.channel) {
+            bot.sendMessage(msg.channel, message);
+          }
+        });
+      } 
+    }
+  },
+
   "setpsn": {
     usage: "[gamertag]",
     description: "Save your PSN name so others can find you on Playstation",
@@ -976,7 +1028,7 @@ var commands = {
         queryUserMeta(id, function(data) {
           var message = "";
           
-          if(data.gamertag == null) {
+          if(data.psn == null) {
             message = "Sorry, I dont have their PSN name. :frowning:";
           }
           else {

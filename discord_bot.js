85f302039fe2dcbe995f2b21c18ac48a2d8157c4
Dustin Essington <dustin.essington@rackspace.com>
2016-08-29 10:04:22 -0500

    add !set3ds and !get3ds

--- discord_bot.js
+++ discord_bot.js
@@ -959,6 +959,58 @@ var commands = {
     }
   },
 
+  "set3ds": {
+    usage: "[0000-1111-2222]",
+    description: "Save your 3DS Friend Code so others can add you on the 3DS",
+    process: function(bot,msg,suffix){
+
+      if(suffix){
+        var user = msg.author.id;
+        setUserMeta(user,'3ds',suffix);
+        bot.sendMessage(msg.channel, 'set your 3DS Friend Code to: ' + suffix);
+      }
+    }
+  },
+
+  "get3ds": {
+    usage: "[@user]",
+    description: "Gets the 3DS Friend Code of a user",
+    process : function(bot,msg,suffix) {
+
+      if(!suffix) {
+        return; // return early, stop execution.
+      }
+
+      var users = suffix.split(' ');
+      
+      for (var i = 0; i < users.length; i++) {
+        
+        var id;
+      
+        // Server nickname detection
+        if(users[i].substr(2, 1) === "!") {
+          id = users[i].slice(3, users[i].length - 1);
+        } else {
+          id = users[i].slice(2, users[i].length - 1);
+        }
+        
+        queryUserMeta(id, function(data) {
+          var message = "";
+          
+          if(data.3ds == null) {
+            message = "Sorry, I dont have their 3DS Friend Code. :frowning:";
+          }
+          else {
+            message = "Here's the 3DS Friend Code for " + tagUser(data) + ":\n" +  data.3ds;
+          }
+          if(msg.channel) {
+            bot.sendMessage(msg.channel, message);
+          }
+        });
+      } 
+    }
+  },
+
   "setpsn": {
     usage: "[gamertag]",
     description: "Save your PSN name so others can find you on Playstation",
@@ -1331,6 +1383,9 @@ var commands = {
 
             if (data.nintendo != null) {
               message = message + "\n" + "Nintendo Network ID: " + data.nintendo;
+            }
+            if (data.3ds != null) {
+              message = message + "\n" + "3DS Friend Code: " + data.3ds;
             }
           }
           if (msg.channel) {

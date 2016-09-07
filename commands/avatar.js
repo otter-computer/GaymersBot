345b52ff3e071d77b80f2f522bbf39457690df78
Thomas Baillie <thomas@thomasshaped.co.uk>
2016-09-07 14:02:14 +0200

    Error check for !avatar 😱

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,7 +3,11 @@ module.exports = {
   description: 'See someone\'s avatar.',
   process: (bot, message) => {
     for (var [id, user] of message.mentions.users) {
-      message.channel.sendMessage(user + '\'s avatar: ' + user.avatarURL);
+      if (user.avatarURL) {
+        message.channel.sendMessage(user + '\'s avatar: ' + user.avatarURL);
+      } else {
+        message.channel.sendMessage(user + ' doesn\'t have an avatar :sob:');
+      }
     }
   }
 };

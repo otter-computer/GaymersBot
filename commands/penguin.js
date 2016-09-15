ec5c296f97d1f2b0a4f7e50aa8bbab2169eae7bb
Thomas Baillie <thomas@thomasshaped.co.uk>
2016-09-15 15:23:39 +0200

    !penguin 🐧

---
+++ commands/penguin.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+module.exports = {
+  usage: '',
+  description: 'Gets a random penguin picture',
+  process: (bot, message) => {
+    // http://penguin.wtf
+    request('http://penguin.wtf', (error, response, body) => {
+      if (error) {
+        console.log('Error: ', error);
+        return;
+      }
+
+      if (response.statusCode !== 200) {
+        console.log('Invalid status code: ', response.statusCode);
+        return;
+      }
+
+      message.reply(body);
+    });
+  }
+};

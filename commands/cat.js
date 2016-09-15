c19f882159e53557c2d14013914332212de6ce2e
Thomas Baillie <thomas@thomasshaped.co.uk>
2016-09-15 15:23:23 +0200

    !cat 🐱

---
+++ commands/cat.js
@@ -0,0 +1,22 @@
+const request = require('request');
+
+module.exports = {
+  usage: '',
+  description: 'Gets a random cat picture',
+  process: (bot, message) => {
+    // http://random.cat/meow
+    request('http://random.cat/meow', (error, response, body) => {
+      if (error) {
+        console.log('Error: ', error);
+        return;
+      }
+
+      if (response.statusCode !== 200) {
+        console.log('Invalid status code: ', response.statusCode);
+        return;
+      }
+
+      message.reply(JSON.parse(body).file);
+    });
+  }
+};

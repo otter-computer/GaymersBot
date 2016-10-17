8c844a588b11e5432ea7dcf9ce860ac9ffdaf2be
ThomasShaped <thomas@thomasshaped.co.uk>
2016-10-17 12:35:44 +0200

    Because Reynbow is a stupid special snowflake cunt

--- commands/joined.js
+++ commands/joined.js
@@ -6,6 +6,16 @@ module.exports = {
   description: 'See when someone joined the server.',
   process: (bot, message) => {
     for (var [id, user] of message.mentions.users) {
+
+      if (id === '123395731548536832') { // If Reynbow.
+
+        var dr = moment('2015-12-27 01:00');
+        var now = dr.fromNow().toUpperCase();
+        message.channel.sendMessage(user.toString() + ' BOUNCED IN HERE LIKE A FEISTY \'ROO ABOUT ' + now + ' (' + dr.format(format).toUpperCase() + '), MATE.');
+
+        return;
+      }
+
       let joinDate = moment(message.guild.member(user).joinDate);
 
       message.channel.sendMessage(user.toString() + ' joined ' + joinDate.fromNow() + ' (' + joinDate.format(format) + ')');

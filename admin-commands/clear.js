8bd62279795bd3e5c708926d8a0772666e089795
eye-sigil <ustin.a.h+github@gmail.com>
2016-11-15 15:27:00 -0500

    Update clear.js

--- admin-commands/clear.js
+++ admin-commands/clear.js
@@ -2,12 +2,13 @@ module.exports = {
   usage: '[amount]',
   description: 'ADMIN ONLY: Clears an amount of messages from chat.',
   process: (bot, message) => {
+    number = message.cleanContent.strip('!');
     try {
-      message.channel.bulkDelete(message.cleanContent);
+      message.channel.bulkDelete(number);
     } catch {
       message.reply('Cannot delete a nonnumber.');
     }
     
-    message.reply('Deleted' + message.cleanContent + 'messages.'
+    message.reply('Deleted' + message.cleanContent + 'messages.')
   }
 }

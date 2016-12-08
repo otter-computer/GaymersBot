5121b10da3c2c5291ed8747af115b48073c189e1
John Marion <jmariondev@gmail.com>
2016-12-07 16:30:02 -0800

    Move nickname changes into their own function
    This allows me to have a role-change handler in memberUpdated.
    Currently, the nickname notifications are disabled so I'm leaving it
    commented but I did confirm that the changed code does work.

--- events/memberUpdated.js
+++ events/memberUpdated.js
@@ -1,31 +1,34 @@
 const moment = require('moment');
 const format = require('../momentFormat');
 
+function usernameUpdate(bot, oldMember, newMember) {
+  let channel = bot.channels.find('name', 'user-logs');
+
+  let oldMemberName, newMemberName;
+
+  if (!oldMember.nickname) {
+    oldMemberName = '**' + oldMember.user.username + '**#' + oldMember.user.discriminator;
+  } else {
+    oldMemberName = '**' + oldMember.nickname + '**#' + oldMember.user.discriminator;
+  }
+
+  if (!newMember.nickname) {
+    newMemberName = '**' + newMember.user.username + '**#' + newMember.user.discriminator;
+  } else {
+    newMemberName = '**' + newMember.nickname + '**#' + newMember.user.discriminator;
+  }
+
+  channel.sendMessage(
+    oldMemberName + ' is now ' + newMemberName + ' ' +
+    '(' + moment(Date.now()).format(format) + ')'
+  );
+}
+
 module.exports = {
-  // Logs a username change in #user-logs.
   process: (bot, guild, oldMember, newMember) => {
-    // Cancel on role (or other user) changes we don't care about
-    if (oldMember.nickname === newMember.nickname) return;
-
-    let channel = bot.channels.find('name', 'user-logs');
-
-    let oldMemberName, newMemberName;
-
-    if (!oldMember.nickname) {
-      oldMemberName = '**' + oldMember.user.username + '**#' + oldMember.user.discriminator;
-    } else {
-      oldMemberName = '**' + oldMember.nickname + '**#' + oldMember.user.discriminator;
-    }
-
-    if (!newMember.nickname) {
-      newMemberName = '**' + newMember.user.username + '**#' + newMember.user.discriminator;
-    } else {
-      newMemberName = '**' + newMember.nickname + '**#' + newMember.user.discriminator;
-    }
-
-    channel.sendMessage(
-      oldMemberName + ' is now ' + newMemberName + ' ' +
-      '(' + moment(Date.now()).format(format) + ')'
-    );
+    // Nickname change
+    //if (oldMember.nickname !== newMember.nickname) {
+    //  usernameUpdate(bot, oldMember, newMember);
+    //}
   }
 };

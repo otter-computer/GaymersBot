6f161b5e2d8c43d66f73c76219e7ba77764ae8ef
John Marion <jmariondev@gmail.com>
2016-12-14 01:53:05 -0800

    Sort role lists alphabetically

--- roles.js
+++ roles.js
@@ -7,32 +7,32 @@ module.exports = {
     'Asia',
     'Europe',
     'Middle East',
-    'Oceania',
     'North America',
+    'Oceania',
     'South America'
   ],
 
   // Roles that cannot be managed with !role
   RESTRICTED_ROLES: [
+    '18+',
     'Admin',
-    'Moderator',
-    'Bots',
+    'Bot Commander',
     'Bot Developer',
-    'Restricted',
     'Bot Restricted',
-    'No links/files',
-    'Under 18',
+    'Bots',
     'DJ',
+    'DiscoBot',
     'ErisBot',
     'Event Manager',
-    'DiscoBot',
-    '18+',
-    'Push-to-talk',
-    'Bot Commander',
-    'Mee6',
-    'Keyboard Warrior',
     'Keyboard GOD!',
-    'Member'
+    'Keyboard Warrior',
+    'Mee6',
+    'Member',
+    'Moderator',
+    'No links/files',
+    'Push-to-talk',
+    'Restricted',
+    'Under 18'
   ],
 
   // A user must have one of these roles to use the bot (unless in a DM)

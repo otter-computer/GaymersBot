c0aee338825898b2355b45886b379002177b3b57
ThomasShaped <thomas@thomasshaped.co.uk>
2016-12-22 14:26:40 +0100

    Changed command name from reserved word
    'status' could possibly be a reserved word, don't wanna take that risk...

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ const commands = {};
 // Import commands
 commands.avatar = require('./commands/avatar');
 commands.boop = require('./commands/boop');
+commands.botstatus = require('./commands/status');
 commands.cat = require('./commands/cat');
 commands.choose = require('./commands/choose');
 commands.help = require('./commands/help');
@@ -62,7 +63,6 @@ commands.set18 = require('./commands/set18');
 commands.setregion = require('./commands/setregion');
 commands.slap = require('./commands/slap');
 commands.spray = require('./commands/spray');
-commands.status = require('./commands/status');
 commands.timeout = require('./commands/timeout');
 commands.unset18 = require('./commands/unset18');
 commands.unsetregion = require('./commands/unsetregion');

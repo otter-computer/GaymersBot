3d6e1232ddec92b919cf5e460f14898bb1174b54
ThomasShaped <thomas@thomasshaped.co.uk>
2016-12-22 13:55:15 +0100

    Add status command

--- .profile
+++
@@ -1,1 +0,0 @@
-export HEROKU_APP_VERSION=${curl -H "Accept: application/vnd.heroku+json; version=3" -H "Authorization: Bearer $HEROKU_API_TOKEN" -n https://api.heroku.com/apps/$HEROKU_APP_NAME/release}
--- commands/status.js
+++ commands/status.js
@@ -4,15 +4,10 @@ module.exports = {
   allowDM: false,
   requireRoles: ['Bot Developer'],
   process: (bot, message) => {
-    if (!process.env.HEROKU_APP_VERSION) {
-      message.reply("Sorry, I couldn't get the latest deploy information. :sob:");
-      return;
-    }
-
-    let commits = JSON.parse(process.env.HEROKU_APP_VERSION);
-
-    let latest = Object.keys(commits)[commits.length - 1];
-
-    message.reply(latest);
+    message.reply(
+      "Bot deployed at: " + process.env.HEROKU_RELEASE_CREATED_AT +
+      "https://github.com/gaymers-discord/DiscoBot/commit/" + process.env.HEROKU_SLUG_COMMIT + "\n" +
+      process.env.HEROKU_SLUG_DESCRIPTION + "\n"
+    );
   }
 };

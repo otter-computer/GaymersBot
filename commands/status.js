38d823a59bfc9770d4ebafb745cc48f7514ebae6
ThomasShaped <thomas@thomasshaped.co.uk>
2016-12-24 12:50:07 +0100

    Added onlyIn
    - Added onlyIn prop
    - Truncated message lines to 80 (ish, I did this on github web editor don't hurt me)

--- commands/status.js
+++ commands/status.js
@@ -3,11 +3,13 @@ module.exports = {
   description: 'BOT DEVELOPER ONLY: Shows the latest deploy from GitHub.',
   allowDM: false,
   requireRoles: ['Bot Developer'],
+  onlyIn: ['discobots-masters'],
   process: (bot, message) => {
     message.reply(
-      'Bot deployed at: `' + process.env.HEROKU_RELEASE_CREATED_AT + '`\n' +
-      'https://github.com/gaymers-discord/DiscoBot/commit/' + process.env.HEROKU_SLUG_COMMIT + '\n' +
-      process.env.HEROKU_SLUG_DESCRIPTION
+      'Bot deployed at: `' + process.env.HEROKU_RELEASE_CREATED_AT
+      + '`\n' + 'https://github.com/gaymers-discord/DiscoBot/commit/'
+      + process.env.HEROKU_SLUG_COMMIT + '\n'
+      + process.env.HEROKU_SLUG_DESCRIPTION
     );
   }
 };

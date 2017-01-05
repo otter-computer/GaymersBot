eabc8996f6fd711bbc9fba8e938dadcc9c722649
ThomasShaped <thomas@thomasshaped.co.uk>
2017-01-05 16:08:50 +0100

    Custom timeout lengths! ⏰

--- commands/timeout.js
+++ commands/timeout.js
@@ -1,20 +1,32 @@
+const Discord = require('discord.js');
 const firebase = require('firebase');
-const moment = require('moment');
-const format = require('../momentFormat');
+const splitargs = require('splitargs');
+const juration = require('juration');
 
 module.exports = {
-  usage: '[@user]',
+  usage: '[@user] [30m]',
   description: 'ADMIN ONLY: Give a user the \'Restricted\' role for 30 minutes. Will also remove the \'18+\' role.',
   allowDM: false,
   requireRoles: ['Admin', 'Moderator'],
   process: (bot, message) => {
-    let timeoutEnd = Date.now() + 1800000;
-
-    const userLogs = bot.channels.find('name', 'user-logs');
+    const userLogsChannel = bot.channels.find('name', 'user-logs');
 
     const restrictedRole = message.guild.roles.find('name', 'Restricted');
     const over18Role = message.guild.roles.find('name', '18+');
     const memberRole = message.guild.roles.find('name', 'Member');
+
+    // Calculate the end time
+    const timeoutStart = Date.now();
+
+    // Let's grab just the time...
+    let timeoutLengthArray = splitargs(message.content);
+    timeoutLengthArray.shift();
+    timeoutLengthArray.shift();
+    let timeoutLength = timeoutLengthArray[0];
+
+    if (!timeoutLength) timeoutLength = '30m';
+
+    const timeoutEnd = timeoutStart + juration.parse(timeoutLength) * 1000;
 
     // Updates to be pushed to firebase
     let updates = {};
@@ -24,58 +36,52 @@ module.exports = {
       return;
     }
 
-    for (var [id, user] of message.mentions.users) {
+    let user = message.guild.member(message.mentions.users.first());
+    let currentRoles = [];
 
-      let member = message.guild.member(user);
-      let currentRoles = [];
+    // Iterate roles to remove 18+ and Member
+    for (let [id, currentRole] of user.roles) {
 
-      // Iterate roles
-      for (let [id, currentRole] of member.roles) {
-
-        // Check for 18+ role
-        if (currentRole === over18Role) {
-          continue;
-        }
-
-        // Leave out the Member role since this makes the restriction to
-        // #appeals not work.
-        if (currentRole === memberRole) {
-          continue;
-        }
-
-        currentRoles.push(currentRole);
+      // Check for 18+ role
+      if (currentRole === over18Role) {
+        continue;
       }
 
-      // Add restricted role
-      currentRoles.push(restrictedRole);
+      // Leave out the Member role since this makes the restriction to
+      // #appeals not work.
+      if (currentRole === memberRole) {
+        continue;
+      }
 
-      // Reapply the roles!
-      member.setRoles(currentRoles);
-
-      // Set timeout release time to now + 30 mins in ms
-      updates['/admin/timeout/' + user.id] = timeoutEnd;
+      currentRoles.push(currentRole);
     }
+
+    // Add restricted role
+    currentRoles.push(restrictedRole);
+
+    // Reapply the roles!
+    user.setRoles(currentRoles);
+
+    // Set timeout release time to now + 30 mins in ms
+    updates['/admin/timeout/' + user.id] = timeoutEnd;
 
     // Push updates to firebase
     firebase.database().ref().update(updates);
 
-    // Confirmation response
-    let response = '';
+    // Send a confirmation message to #user-logs
+    let embed = new Discord.RichEmbed();
 
-    let mentions = message.mentions.users.array();
+    embed.setColor(0xE67E21);
+    embed.setTitle('User Given Timeout');
+    embed.addField('User', user, true);
+    embed.addField('Timeout Length',
+      juration.stringify(juration.parse(timeoutLength)), true);
 
-    if (mentions.length === 1) {
-      response += mentions[0] + ' is';
-    } else {
-      for (let i = 0; i < mentions.length; i++) {
-        response += mentions[i] + ' ';
-      }
-      response += 'are';
-    }
+    embed.setFooter('Timeout End');
 
-    response += ' on a timeout for 30 minutes.';
-    message.reply(response);
+    let embedDate = new Date(timeoutEnd);
+    embed.setTimestamp(embedDate.toISOString());
 
-    userLogs.sendMessage(response + ' (' + moment(Date.now()).format(format) + ') until (' + moment(timeoutEnd).format(format) + ')');
+    userLogsChannel.sendMessage('', { embed: embed });
   }
 };

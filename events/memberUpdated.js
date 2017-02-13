f12219ce425a0261bcbee2fc2ba56d0a1931d024
John Marion <jmariondev@gmail.com>
2017-02-13 00:46:25 -0800

    Clarify that commands should be used in #bot-room
    Completes gaymers-discord/DiscoBot#183

--- events/memberUpdated.js
+++ events/memberUpdated.js
@@ -103,7 +103,7 @@ function memberRoleAdded(newMember) {
     'Please follow our rules. You can find them in the #info-rules channel. \n \n' +
     'If you have any questions you can @admin or @moderator in any channel or PM an admin or moderator directly \n \n' +
     '__**Useful Commands**__ \n' +
-    'These commands can be used in any channel on the server. \n \n' +
+    'These commands can be used in the #bot-room channel on Gaymers. \n \n' +
     '**!help** - Discobot will PM you a complete list of commands. \n' +
     '**!setregion [region]** - Discobot will set your colour based on your region. For example `!setregion Europe` or `!setregion North America` \n' +
     '**!set18** - Discobot will give you access to the #over-18 channel. \n'

5b043acdb6752f0f41b97e37a42f87c039dcd37c
Chris Lennon <chrislennon@users.noreply.github.com>
2017-06-25 01:29:45 +0200

    adds minecraft chat
    adds ability to communicate with the minecraft server in a chat room specified in an environment variable

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ try {
 
   appConfig.AUTH_TOKEN = process.env.AUTH_TOKEN;
   appConfig.APIGW_DISCOBOT_X_API_KEY = process.env.APIGW_DISCOBOT_X_API_KEY;
+  appConfig.MINECRAFT_CHAT_CHANNEL = process.env.MINECRAFT_CHAT_CHANNEL;
   appConfig.SQS_ACCESS_KEY = process.env.SQS_ACCESS_KEY;
   appConfig.SQS_SECRET_KEY = process.env.SQS_SECRET_KEY;
   appConfig.SQS_QUEUE_URL = process.env.SQS_QUEUE_URL;
@@ -100,6 +101,9 @@ process.on('SIGTERM', cleanup);
 
 // Utilities
 const updateAPI = require('./utils/updateAPI');
+
+// APIs
+minecraftChat = require('./api/rcon').mcChat;
 
 // Commands
 const commands = {};
@@ -220,6 +224,11 @@ function commandValidInChannel(command, message) {
 function messageHandler(message) {
   // Ignore bot messages
   if (message.author.bot) {
+    return;
+  }
+
+  if (message.channel.name == appConfig.MINECRAFT_CHAT_CHANNEL) {
+    minecraftChat(message);
     return;
   }
 

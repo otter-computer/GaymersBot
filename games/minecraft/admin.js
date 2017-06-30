dec7ce3f9657530a5d850087bd34922a86044e31
Chris Lennon <chrislennon@users.noreply.github.com>
2017-06-30 21:02:36 +0200

    attempts to clear up the execution flow discussion on https://github.com/gaymers-discord/DiscoBot/pull/306#discussion_r125089276

--- games/minecraft/admin.js
+++ games/minecraft/admin.js
@@ -32,7 +32,7 @@ module.exports = {
     let action = msg[3];
 
     if (msg.length < 4) {
-      message.reply('This command is not valid.')
+      message.reply('This command is not valid.');
       return;
     }
 
@@ -41,37 +41,46 @@ module.exports = {
       return;
     }
 
-
     // Call database check for existance of users_minecraft entry
     minecraft.check(message.mentions.users.first(), function(result, data) {
+      // If the discord user was found in the database, proceed with rcon actions
       if (result){
+        // Get the username from the database callback
         let targetUsername = data[0].minecraft_username;
 
-          rcon.mc_command(targetUsername, action, function(cmdResult, data) {
-
-            if (cmdResult) {
+        // Run the rcon kick/ban/unban action
+        rcon.mc_command(targetUsername, action, function(cmdResult, data) {
+          // If the command was executed on the server, continue
+          if (cmdResult) {
               if (action == 'ban'){
+                // Banning suggests removal from the whitelist
                 rcon.mc_whitelist(targetUsername, 'remove', function() {
-                  message.channel.send(message.mentions.users.first() + ' got a '+action);
+                  message.channel.send(message.mentions.users.first() + ' was banned.');
                 });
               }
-              if (action == 'unban'||action == 'pardon'){
+              else if (action == 'unban'||action == 'pardon'){
+                // Unbanning results in readding the user to the whitelist
                 rcon.mc_whitelist(targetUsername, 'add', function() {
-                  message.channel.send(message.mentions.users.first() + ' got a '+action);
+                  message.channel.send(message.mentions.users.first() + ' was unbanned.');
                 });
               }
-            }
-            else {
-              message.channel.send(message.author + ', Sorry there was an issue processing your command :cry:. ' + data);
-            }
-          });
-
-
+              else if (action == 'kick') {
+                // Kicking does not result in whitelist interaction
+                message.channel.send(message.mentions.users.first() + ' was kicked.');
+              }
+          }
+          // If the rcon execution failed - server is offline or an invalid action was requested
+          else {
+            message.channel.send(message.author +
+              ', Sorry there was an issue processing your command :cry:. ' + data);
+          }
+        });
       }
+      // If the discord user was not registered with the minecraft database
       else{
-        message.channel.send(message.mentions.users.first() + ' is not registered with the minecraft server.');
+        message.channel.send(message.mentions.users.first() +
+          ' is not registered with the minecraft server.');
       }
     });
-
   }
 };

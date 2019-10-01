a1006555b5f628a6eb645c0621c96d5696f4fd3a
Thomas Baillie <thomasbaillie@outlook.com>
2019-10-01 21:05:53 +0200

    Remove logging

--- events/index.js
+++ events/index.js
@@ -1,7 +1,1 @@
-exports.memberBanned = require('./memberBanned');
-exports.memberJoined = require('./memberJoined');
-exports.memberLeft = require('./memberLeft');
-exports.memberUnbanned = require('./memberUnbanned');
-exports.memberUpdated = require('./memberUpdated');
-exports.messageDeleted = require('./messageDeleted');
 exports.messageUpdated = require('./messageUpdated');

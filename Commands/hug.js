64e44ff6463a3b56836b5b2b05d895c9667587ed
Thomas Baillie <thomasbaillie@outlook.com>
2020-04-03 23:41:30 +0200

    Refactor Hug

--- Commands/hug.js
+++ Commands/hug.js
@@ -10,7 +10,34 @@ class Hug extends Command {
   }
 
   execute(Message, ...args) {
+    let target;
 
+    // const mentions = Message.mentions.users;
+
+    if (Message.mentions.users.size > 0) {
+      target = Array.from(Message.mentions.users.values()).join(', ');
+    } else {
+      target = Message.author.toString();
+    }
+
+    const responses = [
+      `*hugs ${target}*`,
+      `*hugs ${target}*`,
+      `*hugs ${target}*`,
+      `*hugs ${target}*`,
+      `*licks ${target}*`,
+      `*pounces ${target}*`,
+      `*jumps on ${target}*`,
+      `*glomps ${target}*`,
+      `*falls on ${target}*`,
+      `*bear hugs ${target}*`,
+      `*tightly squeezes ${target}*`,
+      `*embraces ${target}*`,
+      `*holds ${target} close*`,
+      `*cuddles ${target}*`
+    ];
+
+    Message.channel.send(responses[Math.floor(Math.random() * responses.length)]);
   }
 }
 

72957759bd395be7c18d631f1a668f63d9dc68dd
Thomas Baillie <thomasbaillie@outlook.com>
2020-04-03 23:41:38 +0200

    Refactor Avatar

---
+++ Commands/Avatar.js
@@ -0,0 +1,28 @@
+const Command = require('./Command.js');
+
+class Avatar extends Command {
+  constructor() {
+    super();
+    this.name = 'avatar'
+    this.aliases = ['pic', 'pfp', 'icon'],
+    this.description = 'See a bigger version of someone\'s avatar',
+    this.usage = '@someone',
+    this.serverOnly = true
+  }
+
+  execute(Message, ...args) {
+    let target;
+
+    if (Message.mentions.users.size > 0) {
+      target = Message.mentions.users.first();
+    } else {
+      target = Message.author;
+    }
+
+    const avatarURL = target.displayAvatarURL({dynamic: true});
+
+    Message.reply(`here's ${target.toString()}'s avatar: ${avatarURL}`)
+  }
+}
+
+module.exports = Avatar;

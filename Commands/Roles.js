9895b1525f5c7ef405e96854ce8c42ed23103b56
Thomas Baillie <2439407+ThomasShaped@users.noreply.github.com>
2020-04-06 19:47:22 +0200

    role command & menu

--- Commands/Roles.js
+++ Commands/Roles.js
@@ -5,14 +5,21 @@ const roles = require('../roles.js');
 class Roles extends Command {
   constructor() {
     super();
-    this.name = 'roles'
-    this.aliases = ['role'],
-    this.description = 'Add some roles'
-    this.usage = '',
-    this.serverOnly = true
+    this.name = 'roles';
+    this.aliases = ['role'];
+    this.description = 'Add some roles';
+    this.usage = '';
+    this.serverOnly = true;
   }
 
   execute(Message, ...args) {
+    // Handle specific roles
+    if (!args) {
+      // TODO
+      return;
+    }
+
+    // No roles selected, open role menu
     const embed = new Discord.RichEmbed();
 
     embed.setTitle(`Roles`)
@@ -21,19 +28,41 @@ class Roles extends Command {
       `What type of role would you like to add? Use reactions to select an option.`
     )
 
-    embed.addField('Color','❤️', true)
-    embed.addField('Region','🧡', true)
-    embed.addField('Identity','💛', true)
+    embed.addField('Color', '🌈', true)
+    embed.addField('Region','🗺', true)
+    embed.addField('Identity','🏳️‍🌈', true)
+    embed.addField('Games','🎮', true)
 
     const reactionFilter = (reaction, user) => {
-      return (reaction.emoji.name === '❤️' || reaction.emoji.name === '🧡' || reaction.emoji.name === '💛') && user.id === message.author.id;
+      return user.id === Message.author.id;
     };
 
     Message.reply({embed: embed})
-    .then(() => { Message.react('❤️') })
-    .then(() => { Message.react('🧡') })
-    .then(() => { Message.react('💛') })
-    // .then(() => Message.awaitReactions(filter, { time: 15000 }))
+
+    // Set up collector to listen for reaction response
+    .then(async BotMessage => {
+      await BotMessage.react('🌈');
+      await BotMessage.react('🗺');
+      await BotMessage.react('🏳️‍🌈');
+      await BotMessage.react('🎮');
+
+      const collector = BotMessage.createReactionCollector(reactionFilter, { max: 1, time: 300000 });
+
+      collector.on('collect', (reaction, collector) => {
+        switch (reaction.emoji.name) {
+          case '🌈':
+            this.openColorMenu(Message, BotMessage);
+            break;
+
+          default:
+            break;
+        }
+      })
+    })
+  }
+
+  openColorMenu (Message, BotMessage) {
+
   }
 }
 

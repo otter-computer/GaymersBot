dde927cac26b4d720c273d33baab0889b6379545
Thomas Baillie <thomasbaillie@outlook.com>
2020-04-07 22:33:02 +0200

    Finish dynamic main menu

--- Commands/Roles.js
+++ Commands/Roles.js
@@ -1,12 +1,12 @@
 const Command = require('./Command.js');
 const Discord = require('discord.js');
-// const roles = require('../roles.js');
+const roles = require('../roles.json');
 
 class Roles extends Command {
   constructor() {
     super();
     this.name = 'roles';
-    this.aliases = ['role'];
+    this.aliases = ['role', 'tags', 'color', 'colors', 'colour', 'colours', 'region', 'regions'];
     this.description = 'Add some roles';
     this.usage = '';
     this.serverOnly = true;
@@ -19,42 +19,47 @@ class Roles extends Command {
       return;
     }
 
+    this.openMainMenu(Message);
+  }
+
+  openMainMenu(Message, BotMessage) {
     const menu = [
-      { name: 'Color', description: 'Give yourself a name color', emoji: '🌈' },
-      { name: 'Region', description: 'Let everyone know what region you\'re in', emoji: '🗺' },
-      { name: 'Identity', description: 'Pronouns, sexuality, gender, etc', emoji: '🧬' },
-      { name: 'Platforms', description: 'Set which platforms you play on', emoji: '🖥' },
-      { name: 'Games', description: 'Set which games you play', emoji: '🎮' },
-      { name: 'Server', description: 'Add roles to change server settings', emoji: '🏳️‍🌈' }
+      { name: 'Color', description: 'Give yourself a name color', emoji: '🌈', function: 'openColorMenu' },
+      { name: 'Region', description: 'Let everyone know what region you\'re in', emoji: '🗺', function: 'openRegionMenu' },
+      { name: 'Identity', description: 'Pronouns, sexuality, gender, etc', emoji: '🧬', function: 'openIdentityMenu' },
+      { name: 'Platforms', description: 'Set which platforms you play on', emoji: '🖥', function: 'openPlatformsMenu' },
+      { name: 'Games', description: 'Set which games you play', emoji: '🎮', function: 'openGamesMenu' },
+      { name: 'Server', description: 'Add roles to change server settings', emoji: '🏳️‍🌈', function: 'openServerMenu' }
     ]
 
     // No roles selected, open role menu
-    const embed = new Discord.RichEmbed();
+    const embed = new Discord.MessageEmbed();
 
     embed.setTitle(`Roles`);
 
-    menu.forEach(item => {
+    for (const item of menu) {
       embed.addField(`${item.emoji} ${item.name}`, item.description, true);
-    });
+    }
 
     const reactionFilter = (reaction, user) => {
       return user.id === Message.author.id;
     };
 
+    // TODO: Handle message that already exists
     Message.reply({embed: embed})
 
     // Set up collector to listen for reaction response
-    .then(BotMessage => {
-
-      menu.forEach(async item => {
-        await BotMessage.react(item.emoji);
-      })
-
+    .then(async BotMessage => {
       const collector = BotMessage.createReactionCollector(reactionFilter, { max: 1, time: 300000 });
 
       collector.on('collect', (reaction, collector) => {
-        // TODO: Handle collection
+        const selection = menu.find(item => item.emoji === reaction.emoji.name);
+        this[selection.function]();
       })
+
+      for (const item of menu) {
+        await BotMessage.react(item.emoji);
+      }
     })
   }
 

9597332a154e551f8b9c2e75dc37f7858879af94
Thomas <thomasbaillie@outlook.com>
2020-04-13 11:47:54 +0000

    Fix Spazer's join date

--- commands/joined.js
+++ commands/joined.js
@@ -11,14 +11,22 @@ module.exports = {
       target = message.member;
     }
 
+    const spazerDate = new Date('2016-03-07');
+
     const embed = new Discord.RichEmbed();
     embed.setColor(0x3398DB);
 
-    embed.setTitle(
-      target.displayName +
-      ' joined ' +
-      moment(target.joinedAt).fromNow()
-    );
+    if (target.id === '120897878347481088') {
+      embed.setTitle(
+        `${target.displayName} joined ${moment(spazerDate).fromNow()}`
+      );
+    } else {
+      embed.setTitle(
+        target.displayName +
+        ' joined ' +
+        moment(target.joinedAt).fromNow()
+      );
+    }
 
     embed.setAuthor(
       target.displayName,
@@ -28,7 +36,12 @@ module.exports = {
 
     embed.addField('User:', target, true);
     embed.addField('Requested By:', message.author, true);
-    embed.addField('Date:', target.joinedAt, true);
+
+    if (target.id === '120897878347481088') {
+      embed.addField('Date:', spazerDate, true);
+    } else {
+      embed.addField('Date:', target.joinedAt, true);
+    }
 
     message.channel.send(
       'Here\'s ' +

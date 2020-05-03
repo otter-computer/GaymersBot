967c6c8d392907caad32fce0f9c965cd912c4577
Thomas Baillie <thomasbaillie@outlook.com>
2020-05-03 19:22:33 +0200

    Add iplay command

--- Commands/IPlay.js
+++ Commands/IPlay.js
@@ -11,29 +11,71 @@ class IPlay extends Command {
     this.usage = ``;
     this.serverOnly = true;
     this.gameRoles = [];
+    this.gameRolesLowerCase = [];
   }
 
-  execute(Message) {
+  async execute(Message) {
     if (this.gameRoles.length === 0) this.updateRoles(Message);
 
-    // TODO: add refresh command
-    // TODO: Add/remove chosen role
+    const args = Message.content.split(/ +/);
+    args.shift();
+
+    if (args.length === 0) {
+      const content = `Here's a list of game roles you can add. These roles are pingable by anyone on the server. Add a role like this: \`!iplay World of Warcraft\``;
+      const embed = new Discord.MessageEmbed();
+      embed.setDescription(this.gameRoles.join(`\n`));
+      Message.reply(content, {embed: embed, split: true});
+      return;
+    }
+
+    if (args[0] === `refresh`) {
+      Message.reply(`updating internal role list.`);
+      this.updateRoles(Message);
+      return;
+    }
+
+    const gameRoleName = args.join(` `);
+
+    if (this.gameRolesLowerCase.includes(gameRoleName.toLowerCase())) {
+      const Member = await Message.guild.members.fetch(Message.author.id);
+      const Role = await Message.guild.roles.cache.find(role => role.name.toLowerCase() === gameRoleName);
+
+      if (!Role || !Member) return;
+
+      if (Member.roles.cache.find(role => role.name.toLowerCase() === gameRoleName.toLowerCase())) {
+        Member.roles.remove(Role);
+        Message.reply(`I've removed the ${gameRoleName} role from you :ok_hand:`);
+      } else {
+        Member.roles.add(Role);
+        Message.reply(`I've added the ${gameRoleName} role to you :ok_hand: Remember, this role is pingable by anyone on the server. To remove it, run this command again.`);
+      }
+    } else {
+      const content = `sorry, there isn't a game role that matches that. Here's a list of game roles you can add. These roles are pingable by anyone on the server. Add a role like this: \`!iplay World of Warcraft\``;
+      const embed = new Discord.MessageEmbed();
+      embed.setDescription(this.gameRoles.join(`\n`));
+      Message.reply(content, {embed: embed, split: true});
+      return;
+    }
   }
 
   updateRoles(Message) {
     const nonGameRoles = roles.nonGameRoles;
     const guildRoles = Message.guild.roles.cache.array();
     const gameRoles = [];
+    const gameRolesLowerCase = [];
 
     for (const role of guildRoles) {
       if (nonGameRoles.includes(role.name)) continue;
       if (role.name === `@everyone`) continue;
       gameRoles.push(role.name);
+      gameRolesLowerCase.push(role.name.toLowerCase());
     }
 
     gameRoles.sort();
+    gameRolesLowerCase.sort();
 
     this.gameRoles = gameRoles;
+    this.gameRolesLowerCase = gameRolesLowerCase;
   }
 }
 

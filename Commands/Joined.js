9ea74db41fc2ad9827508ddaeabfe150cc376a58
Thomas Baillie <thomasbaillie@outlook.com>
2020-05-06 21:44:49 +0200

    Fix joined, should target member not user

--- Commands/Joined.js
+++ Commands/Joined.js
@@ -13,13 +13,13 @@ class Joined extends Command {
   }
 
   execute(Message) {
-    const target = Message.mentions.users.first() ? Message.mentions.users.first() : Message.member;
+    const target = Message.mentions.members.first() ? Message.mentions.members.first() : Message.member;
     const date = target.id === `120897878347481088` ? new Date(`2016-03-07`) : target.joinedAt;
 
     const embed = new Discord.MessageEmbed();
     embed.setColor(0x3398DB);
 
-    embed.setTitle(`${target.tag} joined ${moment(date).fromNow()}`);
+    embed.setTitle(`${target.displayName} joined ${moment(date).fromNow()}`);
     embed.addField(`Date:`, date, true);
     embed.setTimestamp(date);
 

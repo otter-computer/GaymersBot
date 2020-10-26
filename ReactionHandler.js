aaa01eaa647ff2db7764d2f39df9dbc3e00f16ab
br0keN <jorhorc@gmail.com>
2020-10-25 20:01:18 -0500

    Initial commit! Basic tracking and cross-posting

--- ReactionHandler.js
+++ ReactionHandler.js
@@ -1,5 +1,8 @@
 const Discord = require(`discord.js`);
 const roles = require(`./roles`);
+const START_TRESHOLD = 2;
+const STARBOARD_ID = '770059932635889695';
+const STARBOARD18_ID = '770059902311727116';
 
 class ReactionHandler {
   constructor() {
@@ -39,6 +42,10 @@ class ReactionHandler {
       this.handleRoleReaction(Reaction, User, args[0]);
       return;
     }
+    else if (Reaction._emoji.name === '⭐' && Reaction.message.type === 'DEFAULT' && Reaction.message.author.bot === false) {
+      // Is it the star emoji on a non-system, non-bot message, let's handle it
+      this.handleStarReaction(Reaction, args[0]);
+    }
   }
 
   async handleRoleReaction(Reaction, User, type) {
@@ -63,6 +70,38 @@ class ReactionHandler {
 
     if (type === `ADD`) Member.roles.add(Role);
     if (type === `REMOVE`) Member.roles.remove(Role);
+  }
+
+  async handleStarReaction(Reaction) {
+    await Reaction.fetch();
+    
+    // TODO: Any channels we don't want to count star reactions on (like Announcements, etc.)?
+    // TODO: Do we want to use permissions to prevent emoji on starboard channels?
+    
+    if (Reaction.message.channel.name !== 'starboard' && Reaction.message.channel.name !== 'starboard-over-18'
+        && Reaction.count >= START_TRESHOLD) {
+      
+      var starMessage = Reaction.message.author.toString() + ' [Everyone liked that]\nHere\'s the message: \n\n' + Reaction.message.content;
+      
+      if (Reaction.message.channel.parent != null && Reaction.message.channel.parent.name === 'Over 18') {
+        // Is it probably a naughty message?
+        Reaction.client.channels.fetch(STARBOARD18_ID)
+        .then(channel => channel.send(starMessage))
+        .catch(console.error);
+      }
+      else {
+        // It's a message anyone should be able to see
+        Reaction.client.channels.fetch(STARBOARD_ID)
+        .then(channel => channel.send(starMessage))
+        .catch(console.error);
+      }
+    }
+
+    // TODO: Avoid posting the message a million times... perhaps ignore when over threshold?
+    // TODO: ...and handle someone meeting threshold and quickly removing star (maybe)
+    // TODO: A few magic strings here, const-ify probs
+
+    return;
   }
 }
 

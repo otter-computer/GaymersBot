f473665336566cac77049fc250537776af75f8fd
Thomas Baillie <thomas@otter.pizza>
2020-11-01 13:51:07 +0100

    Convert to dynamic help command, use rich embed builder

--- Commands/Help.js
+++ Commands/Help.js
@@ -1,43 +1,46 @@
 const Command = require(`./Command.js`);
+const fs = require(`fs`);
+const Discord = require('discord.js');
 
 class Help extends Command {
   constructor() {
     super();
     this.name = `help`;
     this.aliases = [`h`, `commands`, `halp`];
-    this.description = `View available commands the bot can handle`;
+    this.description = `View available commands the bot can handle.`;
     this.usage = ``;
     this.serverOnly = true;
+
+    // Dynamically load commands
+    // I know this means commands are loaded twice
+    // TODO: inherit these from the MessageHandler when Help is instantiated
+    this.commands = new Discord.Collection();
+
+    const commandFiles = fs.readdirSync(`./Commands`)
+      .filter(file => file.endsWith(`.js`))
+      .filter(file => file !== `Command.js`)
+      .filter(file => file !== `Help.js`)
+      .map(file => require(`../Commands/${file}`))
+      .filter(cmd => cmd.name)
+      .forEach(cmd => this.commands.set(cmd.name.toLowerCase(), new cmd()), this);
   }
 
   async execute(Message) {
-        Message.channel.send({embed: {
-            color: `#d74894`,
-            author: {
-              name: Message.client.user.username,
-              icon_url: `https://cdn.discordapp.com/avatars/${Message.client.user.id}/${Message.client.user.avatar}.png`
-            },
-            title: `Here are the available Kirby commands`,
-            description: `Tip: for all commands do not include the square brackets "[]"`,
-            fields: [{
-                name: `!iplay [game]`,
-                value: `Tell us which games you play! This will add a pingable role so you can find other people to play with`
-              },
-              {
-                name: `!iplay`,
-                value: `See the list of available game roles`
-              },
-              {
-                name: `!joined [@someone]`,
-                value: `See when someone joined the server`
-              },
-              {
-                name: `!avatar [@someone]`,
-                value: `See a bigger version of someone's avatar`
-              }
-            ]
-          }
-        });
+    const embed = new Discord.MessageEmbed();
+
+    embed.setColor(0xD74894);
+
+    const commands = this.commands
+    .filter(command => !command.staffOnly)
+    .filter(command => !command.disabled);
+
+    commands.forEach(command => {
+      embed.addField(`!${command.name} ${command.usage}`, command.description, false);
+    });
+
+    embed.setFooter(`Square brackets [] mean this part is optional. You don't need to include the brackets themselves in the command.`)
+
+    Message.reply(`Here's the commands you can use:`, {embed: embed});
   }
 }
 

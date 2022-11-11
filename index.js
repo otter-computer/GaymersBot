c0aab283696b5dc84f6492042e49e137d90ad10c
Thomas <thomas@otter.computer>
2022-11-11 20:04:35 +0000

    Remove docker

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ function cleanup() {
 }
 
 // Basic HTTP server to keep Azure App Service happy
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('GaymersBot is alive!');
-}).listen(80);
+if (process.env.NODE_ENV === `production`) {
+  http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('GaymersBot is alive!');
+  }).listen(80);
+}
 
 GaymersBot.connect();

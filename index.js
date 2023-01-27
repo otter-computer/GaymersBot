849d05a2ad726fcda966e2bf4aa62b8ceaa4d55c
Thomas <thomas@otter.computer>
2023-01-27 14:58:02 +0000

    Remove http server

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const http = require('http');
 const Bot = require(`./Bot`);
 
 const GaymersBot = new Bot();
@@ -10,14 +9,6 @@ process.on(`SIGTERM`, cleanup);
 function cleanup() {
   GaymersBot.destroy();
   process.exit();
-}
-
-// Basic HTTP server to keep Azure App Service happy
-if (process.env.NODE_ENV === `production`) {
-  http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('GaymersBot is alive!');
-  }).listen(8080);
 }
 
 GaymersBot.connect();
